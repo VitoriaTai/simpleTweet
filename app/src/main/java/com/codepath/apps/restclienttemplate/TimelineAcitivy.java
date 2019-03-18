e2527aadb3386753455eebb3c224dcda7c9cabbd
vitoriatai <vitoria.tai63@gmail.com>
2019-03-17 22:58:41 -0400

    Part 2 od SimpleTweet activity

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineAcitivy.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineAcitivy.java
@@ -1,11 +1,17 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Intent;
+import android.os.Parcel;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -13,6 +19,7 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +28,7 @@ import cz.msebera.android.httpclient.Header;
 
 public class TimelineAcitivy extends AppCompatActivity {
 
+    private final int REQUESTED_CODE = 20;
     private TwitterClient client;
     private RecyclerView rvTweet;
     private TweetsAdapter adapter;
@@ -62,6 +70,32 @@ public class TimelineAcitivy extends AppCompatActivity {
                 populateHomeTimeline();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.compose){
+            Intent i = new Intent(this, ComposeAcitivity.class);
+            startActivityForResult(i, REQUESTED_CODE);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUESTED_CODE && resultCode == RESULT_OK){
+           Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
+           tweets.add(0, tweet);
+           adapter.notifyItemInserted(0);
+        }
     }
 
     private void populateHomeTimeline(){
